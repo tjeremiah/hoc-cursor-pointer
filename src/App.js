14585a002eb1672f3b4c96f10147f1737c07e4e4
tjeremiah <taurel107@gmail.com>
2025-08-06 16:28:52 -0400

    implemented HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
 
+//implementing HOC for mousepointer
+const withMousePosition = (WrappedComponent) => {
+  return (props) => {
+    return <WrappedComponent {...props} />
+  }
+}
+
+const PanelMouseLogger = ({mousePosition}) => {
+  if (!mousePosition) {
+    return null;
+  }
+  return (
+    <div className="BasicTracker">
+      <p>Mouse position:</p>
+      <div className="Row">
+        <span>x: {mousePosition.x}</span>
+        <span>y: {mousePosition.y}</span>
+      </div>
+    </div>
+  );
+};
+
+const PointMouseLogger = ({ mousePosition}) => {
+  if (!mousePosition) {
+    return null;
+  }
+  return (
+    <p>
+      ({mousePosition.x}, {mousePosition.y})
+    </p>
+  );
+};
 
 
 function App() {
   return (
-    
+    <div className="App">
+      <header className="Header">Little Lemon Restaurant 🍕</header>
+      <PanelMouseLogger />
+      <PointMouseLogger />
+
+    </div>
   );
 }
 
